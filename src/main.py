f739e928b5c668a7aef2523a3e0a00b37ed565b9
ignurof <ignurof@ignurof.com>
2024-05-02 10:58:03 +0200

    Update run with extended api
    Add handle_error
    Update parse_meminfo into search function

--- src/main.py
+++ src/main.py
@@ -1,30 +1,35 @@
 import sys
 
+
 def init():
     args = sys.argv[1:]
     run(args)
 
+
 def run(args):
     match args[0]:
         case 'total':
-            result = get_total()
+            [msg, result] = get_total()
         case 'available':
-            result = get_available()
+            [msg, result] = get_available()
+        case _:
+            [msg, result] = handle_error(args[0])
 
+    print(msg) 
     return result
 
 
-def parse_meminfo():
+def handle_error(arg):
+    return ['error', 1]
+
+
+def parse_meminfo(value):
     with open('/proc/meminfo', 'r') as file:
         lines = file.readlines()
-        data = []
         for line in lines:
-            values = line.split(':')
-            values[1] = values[1].strip()
-            values[1] = values[1].rstrip('kB ')
-            data.append((values[0], values[1]))
-
-        return data
+            if value in line:
+                return list(filter(None, line.split(' ')))[1] 
+       
 
 def convert_kb_to_gb(value):
     return float(value) * 10**-6
@@ -35,8 +40,12 @@ def parse_input(user_input):
         if user_input in data[0]:
             print('Key: {key} | Value: {value}'.format(key = data[0], value = data[1]))
 
+
 def get_total():
-    return 'Total'
+    total = parse_meminfo('MemTotal')
+    return [f'Total Memory: {total}', 0] 
+
 
 def get_available():
-    return 'Available'
+    available = parse_meminfo('MemAvailable')
+    return [f'Available Memory: {available}', 0] 
--- src/test.py
+++ src/test.py
@@ -3,12 +3,21 @@ import main
 
 class Test(unittest.TestCase):
     def test_total(self):
-        self.assertEqual(main.run(['total']), 'Total')
+        self.assertEqual(main.run(['total']), 0)
 
     def test_available(self):
-        self.assertEqual(main.run(['available']), 'Available')
+        self.assertEqual(main.run(['available']), 0)
+
+    def test_handle_empty_string(self):
+        self.assertEqual(main.run(['']), 1)
+
+    def test_handle_invalid_string(self):
+        self.assertEqual(main.run(['blablab']), 1)
+
 
 if __name__ == '__main__':
     t = Test()
     t.test_total()
     t.test_available()
+    t.test_handle_empty_string()
+    t.test_handle_invalid_string()
