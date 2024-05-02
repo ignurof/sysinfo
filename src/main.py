f9806ecc2d015f338d9a8424fc3397a3d50efaeb
ignurof <ignurof@ignurof.com>
2024-05-02 08:02:08 +0200

    Add /proc/meminfo reading and parsing

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
 def run():
     print("Hello, World")
+    print_meminfo()
+
+def parse_meminfo():
+    with open('/proc/meminfo', 'r') as file:
+        lines = file.readlines()
+        data = []
+        for line in lines:
+            values = line.split(':')
+            values[1] = values[1].strip()
+            values[1] = values[1].rstrip('kB ')
+            data.append((values[0], values[1]))
+
+        return data
+
+def print_meminfo():
+    meminfo = parse_meminfo()
+    for data in meminfo:
+        print(data)
