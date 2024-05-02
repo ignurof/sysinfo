c86bcf125be0c755a7b7efb504356f5f88e3fd46
ignurof <ignurof@ignurof.com>
2024-05-02 08:46:51 +0200

    Add meminfo ram display
    Also includes kb to gb conversion.
    Mockup for user input.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def run():
     print("Hello, World")
-    print_meminfo()
+    print_ram()
+    parse_input('DirectMap1G')
 
 def parse_meminfo():
     with open('/proc/meminfo', 'r') as file:
@@ -14,7 +15,20 @@ def parse_meminfo():
 
         return data
 
-def print_meminfo():
+def print_ram():
     meminfo = parse_meminfo()
     for data in meminfo:
-        print(data)
+        if 'MemTotal' in data[0]:
+            print('Total RAM: {:.2f} GB'.format(convert_kb_to_gb(data[1]))) 
+        elif 'MemAvailable' in data[0]:
+            print('Available RAM: {:.2f} GB'.format(convert_kb_to_gb(data[1]))) 
+
+
+def convert_kb_to_gb(value):
+    return float(value) * 10**-6
+
+
+def parse_input(user_input):
+    for data in parse_meminfo():
+        if user_input in data[0]:
+            print('Key: {key} | Value: {value}'.format(key = data[0], value = data[1]))
